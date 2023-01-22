1fa757b0fb2ce3fd121eb22e4865e7910484ea1c
Origami <84212308+OrigamiGamer@users.noreply.github.com>
2023-01-22 16:15:13 +0800

    big update

---
+++ ChaosLanguage/test.cpp
@@ -0,0 +1,30 @@
+class Person{
+	string GetName(){
+		return("name");
+	}
+}
+
+class Object{
+	Person Origami
+	Person Get(){
+		return(Origami);
+		if(){
+
+		};
+	}
+	void method1(){}
+	void method2(){}
+	void method3(){}
+}
+
+void main(){
+
+	Object obj;
+
+
+
+
+	obj.Get().GetName();
+
+
+}
---
+++ Engine/ChaosEngine.js
@@ -0,0 +1,27 @@
+{
+
+    // 定义接口类
+    class ChaosEngine{
+        
+        init(){
+        
+        }
+        
+        call(InterfaceName = String){
+            
+        }
+
+    }
+
+
+    // 全局变量
+    var Engine = new ChaosEngine();
+
+    Engine.call();
+
+
+
+
+
+
+}
